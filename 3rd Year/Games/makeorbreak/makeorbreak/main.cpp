606f119f3c47799d6646b550cc4ccab40c03bbab
Jon E Rankin <jr0436@bris.ac.uk>
2014-03-26 14:47:07 +0000

    Initial Commit

---
+++ 3rd Year/Games/makeorbreak/makeorbreak/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  makeorbreak
+//
+//  Created by Jon E Rankin on 26/03/2014.
+//  Copyright (c) 2014 Jon E Rankin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
